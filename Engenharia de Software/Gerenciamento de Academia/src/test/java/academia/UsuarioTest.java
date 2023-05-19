e9532f0b3a4126ac1a8b2d42aee9d4a05537dbe8
pabloo-cunha <pablo.limacunha@outlook.com>
2023-05-19 19:43:36 -0300

    Novo programa

---
+++ Engenharia de Software/Gerenciamento de Academia/src/test/java/academia/UsuarioTest.java
@@ -0,0 +1,48 @@
+package academia;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsuarioTest {
+
+    @Test
+    void getNome() {
+    }
+
+    @Test
+    void setNome() {
+    }
+
+    @Test
+    void getIdade() {
+    }
+
+    @Test
+    void setIdade() {
+    }
+
+    @Test
+    void getAltura() {
+    }
+
+    @Test
+    void setAltura() {
+    }
+
+    @Test
+    void getPesoAtual() {
+    }
+
+    @Test
+    void setPesoAtual() {
+    }
+
+    @Test
+    void getMetaDePeso() {
+    }
+
+    @Test
+    void setMetaDePeso() {
+    }
+}
