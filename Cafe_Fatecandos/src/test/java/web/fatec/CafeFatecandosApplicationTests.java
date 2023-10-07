8e8298b9ab6c7021989ecd3b84253c581ee4686b
pabloo-cunha <pablo.limacunha@outlook.com>
2023-10-06 21:57:50 -0300

    :tada: First commit

---
+++ Cafe_Fatecandos/src/test/java/web/fatec/CafeFatecandosApplicationTests.java
@@ -0,0 +1,13 @@
+package web.fatec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeFatecandosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- Engenharia de Software/Gerenciamento de Academia/src/test/java/academia/AcademiaTest.java
+++ Engenharia de Software/Gerenciamento de Academia/src/test/java/academia/AcademiaTest.java
@@ -21,12 +21,16 @@ class AcademiaTest {
         academia.cadastrarUsuarios("Pablo", 27, 1.65, 67.0, 65);
         academia.cadastrarUsuarios("João", 29, 1.85, 60.0, 65);
         academia.cadastrarUsuarios("Maria", 27, 1.65, 67.0, 67.0);
+        academia.cadastrarUsuarios("Carlos", 28, 1.70, 78.0, 75);
+        academia.cadastrarUsuarios("Carol", 23, 1.80, 90.0, 80);
 
         List<Usuario> cadastro = academia.getCadastros();
-        assertEquals(3, cadastro.size());
+        assertEquals(5, cadastro.size());
         assertEquals("Pablo", cadastro.get(0).getNome());
         assertEquals("João", cadastro.get(1).getNome());
         assertEquals("Maria", cadastro.get(2).getNome());
+        assertEquals("Carlos", cadastro.get(3).getNome());
+        assertEquals("Carol", cadastro.get(4).getNome());
     }
 
     @Test
